f52711604d02d5feed18fb9662899b17b03be1a2
DImentiy1697 <pereganichd@gmail.com>
2026-08-08 01:09:09 +0300

    Update data.js

--- data.js
+++ data.js
@@ -28,31 +28,78 @@ window.ARS_DATA = {
         progressPercent: 0
     },
 
-    /*
-       ДОДАВАЙ НОВИХ УЧАСНИКІВ СЮДИ.
-       Приклад:
-       { username: "@nickname", club: "Real Madrid" },
+    /participants: [
+    { username: "@dimentiy19", club: "Реал Мадрид" },
+    { username: "@Mbappe_501", club: "ПСЖ" },
+    { username: "@MehrabyanRaffi", club: "Барселона" },
+    { username: "@Vetal6h", club: "Милан" },
+    { username: "@kentyxa26", club: "МЮ" },
+    { username: "@Viteaqa", club: "Арсенал" },
+    { username: "@Limbo_66", club: "Реал Мадрид 2" },
+    { username: "@FCSMilovereal", club: "ПСЖ 2" },
+    { username: "@WondySale", club: "Манчестер Сити" },
+    { username: "@asvlss", club: "Манчестер Сити 2" },
+    { username: "@spakserega4", club: "Барселона 2" },
+    { username: "@terehjuve", club: "Ювентус" },
+    { username: "@shapus2", club: "Ювентус 2" },
+    { username: "@MishaBurkoveckii", club: "Боруссия Дортмунд" },
+    { username: "@Lalitovv", club: "Боруссия Дортмунд 2" },
+    { username: "@No_Name_KZ08", club: "Бавария" },
+    { username: "@Andfoza", club: "Ливерпуль" },
+    { username: "@Yeuwubs", club: "Базель" },
+    { username: "@Oraxan", club: "Ливерпуль 2" },
+    { username: "@tsavaaaa", club: "Атлетико Мадрид" },
+    { username: "@V_llaa_d", club: "Лилль" },
+    { username: "@finallygetaway", club: "Интер" },
+    { username: "@Liverpoolfc1892i", club: "Арсенал 2" },
+    { username: "@destrooooo12", club: "Бавария 2" }
+],*
 
-       Лічильник 0/24 на сайті рахується автоматично.
-    */
-    participants: [],
 
     /*
-       ПІСЛЯ ЖЕРЕБКУВАННЯ розкладай команди по групах.
-       В кожній групі максимум 4 команди.
-       Приклад:
-       A: [
-           { username: "@nickname", club: "Real Madrid" }
-       ]
-    */
-    groups: {
-        A: [],
-        B: [],
-        C: [],
-        D: [],
-        E: [],
-        F: []
-    },
+ groups: {
+    A: [
+        { username: "@shapus2", club: "Ювентус 2" },
+        { username: "@FCSMilovereal", club: "ПСЖ 2" },
+        { username: "@Lalitovv", club: "Боруссия Дортмунд 2" },
+        { username: "@V_llaa_d", club: "Лилль" }
+    ],
+
+    B: [
+        { username: "@Mbappe_501", club: "ПСЖ" },
+        { username: "@Limbo_66", club: "Реал Мадрид 2" },
+        { username: "@Oraxan", club: "Ливерпуль 2" },
+        { username: "@spakserega4", club: "Барселона 2" }
+    ],
+
+    C: [
+        { username: "@WondySale", club: "Манчестер Сити" },
+        { username: "@destrooooo12", club: "Бавария 2" },
+        { username: "@kentyxa26", club: "МЮ" },
+        { username: "@terehjuve", club: "Ювентус" }
+    ],
+
+    D: [
+        { username: "@MishaBurkoveckii", club: "Боруссия Дортмунд" },
+        { username: "@MehrabyanRaffi", club: "Барселона" },
+        { username: "@tsavaaaa", club: "Атлетико Мадрид" },
+        { username: "@No_Name_KZ08", club: "Бавария" }
+    ],
+
+    E: [
+        { username: "@finallygetaway", club: "Интер" },
+        { username: "@Liverpoolfc1892i", club: "Арсенал 2" },
+        { username: "@Yeuwubs", club: "Базель" },
+        { username: "@Viteaqa", club: "Арсенал" }
+    ],
+
+    F: [
+        { username: "@asvlss", club: "Манчестер Сити 2" },
+        { username: "@Andfoza", club: "Ливерпуль" },
+        { username: "@dimentiy19", club: "Реал Мадрид" },
+        { username: "@Vetal6h", club: "Милан" }
+    ]
+},
 
     history: [
         {
